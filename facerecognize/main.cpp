d9f3e2628900f72f7dfaf306a82fd9ef7493b753
vath <346308443@qq.com>
2020-10-17 03:56:46 +0800

    5_features_over

--- facerecognize/main.cpp
+++ facerecognize/main.cpp
@@ -1,29 +1,50 @@
 #include <iostream>
 #include <opencv.hpp>	
 
-#include <seeta\FaceDetector.h>
+#include <seeta/FaceDetector.h>
+#include <seeta/FaceLandmarker.h>
 
 using namespace cv;
 using namespace std;
 
 const char *FD_path = "C:\\Users\\34630\\Desktop\\seetaface6\\sf6.0_windows\\model\\sf3.0_models\\face_detector.csta";
+const char *FL_path = "C:\\Users\\34630\\Desktop\\seetaface6\\sf6.0_windows\\model\\sf3.0_models\\face_landmarker_pts5.csta";
 
+//标识五个特征点
+void mark(seeta::FaceLandmarker *fl, const SeetaImageData &image, const SeetaRect &face,Mat canvas) {
+	std::vector<SeetaPointF> points = fl->mark(image, face);
+	for (auto &point : points) {
+		std::cout << "[" << point.x << ", " << point.y << "]" << std::endl;
+		Point p;  //seetaPoit 转cvpoint
+		p.x = point.x;
+		p.y = point.y;
+		circle(canvas, p, 3, Scalar(0, 0, 255), -1); //画出5点位置
+	}
+};
 
 int main()
 {
-	//Mat img = imread("C:\\Users\\34630\\Desktop\\seetaface6\\facerecognize\\image\\ym2.jpg");
-	//imshow("", img);
-	//waitKey(0);
-	//return 0;
+	
 	int device_id = 0;
+
+	//人脸检测构造器
 	seeta::ModelSetting FD_model;
 	FD_model.append(FD_path);
 	FD_model.set_device(seeta::ModelSetting::CPU);
 	FD_model.set_id(device_id);
-
 	seeta::FaceDetector FD(FD_model);
+	cout << "FD finished" << endl;
 
-	cout << "FD初始化成功过" << endl;
+	//关键点构造器
+
+	seeta::ModelSetting FL_model;
+	FL_model.append(FL_path);
+	FL_model.set_device(seeta::ModelSetting::CPU);
+	FL_model.set_id(device_id);
+	seeta::FaceLandmarker FL(FL_model);
+	cout << "Facelank finished" << endl;
+
+
 
 	Mat frame = imread("C:\\Users\\34630\\Desktop\\seetaface6\\facerecognize\\image\\ym2.jpg");
 	Mat canvas;
@@ -36,10 +57,13 @@ int main()
 	simg.channels = frame.channels();
 	simg.data = frame.data;
 
-	auto infos = FD.detect(simg);
 
+	SeetaRect rect;
+	//确定人脸位置
+	auto infos = FD.detect(simg);
 	int line_width = 4;
 	for (int i = 0; i < infos.size; i++) {
+		rect = infos.data[i].pos;
 		float scale = infos.data[i].pos.width / 300.0;
 		cv::rectangle(canvas,
 			cv::Rect(infos.data[i].pos.x, infos.data[i].pos.y,
@@ -47,7 +71,10 @@ int main()
 			cv::Scalar(128, 0, 0),
 			scale *line_width
 		);
-	}
+	};
+
+	mark(&FL,simg,rect,canvas);
+
 	namedWindow("detected_image");
 	imshow("detected_image", canvas);
 	waitKey(0);
