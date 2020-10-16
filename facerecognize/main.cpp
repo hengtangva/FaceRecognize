d884a89d3c93f1227d2098fff93f151ccd74d349
vath <346308443@qq.com>
2020-10-17 00:48:23 +0800

    update_opecv_over

--- facerecognize/main.cpp
+++ facerecognize/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <opencv.hpp>		
+#include <opencv.hpp>	
+
 //#include <opencv2/opencv.hpp>
 using namespace cv;
 
 int main()
 {
 	Mat img = imread("C:\\Users\\34630\\Desktop\\seetaface6\\facerecognize\\image\\ym2.jpg");
-	imshow("ԭͼ", img);
+	imshow("", img);
 
 	waitKey(0);
 	return 0;
